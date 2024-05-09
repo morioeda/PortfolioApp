784b4ba861bf074a991e473d4030ccd09538f89e
mori.oeda <mori.oeda@fonfun.co.jp>
2024-05-09 15:46:05 +0900

    ユーザー新規登録ファイルの追加

---
+++ src/main/java/com/example/portfolioapp/UserInfoController.java
@@ -0,0 +1,32 @@
+package com.example.portfolioapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserInfoController {
+	
+    /**
+     * ユーザー新規登録画面を表示
+     * @param model Model
+     * @return ユーザー情報一覧画面
+     */
+    @GetMapping(value = "/user/add")
+    public String displayAdd(Model model) {
+        model.addAttribute("userAddRequest", new UserAddRequest());
+        return "";
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
---
+++ src/main/java/com/example/portfolioapp/dto/UserAddRequest.java
@@ -0,0 +1,5 @@
+package com.example.portfolioapp.dto;
+
+public class UserAddRequest {
+
+}
