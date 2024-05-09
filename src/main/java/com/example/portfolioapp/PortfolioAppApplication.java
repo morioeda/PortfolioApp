9d5b3061c9ee21cc40584d5f975a631821bf7d6d
mori.oeda <mori.oeda@fonfun.co.jp>
2024-05-09 10:55:44 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/portfolioapp/PortfolioAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAppApplication.class, args);
+	}
+
+}
