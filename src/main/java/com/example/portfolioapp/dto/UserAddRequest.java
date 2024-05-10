878488a046b428dfd0ee54359f0d8889f6c2b2fe
mori.oeda <mori.oeda@fonfun.co.jp>
2024-05-10 19:36:26 +0900

    CSSファイルへの追記（フォームの修正、フッターの追加）

--- src/main/java/com/example/portfolioapp/dto/UserAddRequest.java
+++ src/main/java/com/example/portfolioapp/dto/UserAddRequest.java
@@ -16,12 +16,12 @@ import lombok.Data;
 public class UserAddRequest implements Serializable {
 
      //名前
-    @NotEmpty(message = "名前を入力してください")
-    @Size(max = 100, message = "名前は100字以内で入力してください")
+    @NotEmpty(message = "名前は必ず入力してください")
+    @Size(max = 100, message = "名前は255文字以内で入力してください")
     private String name;
 
      //メールアドレス
-    @Email
+    @Email(message = "メールアドレスが正しい形式ではありません")
     private String email;
     
     // 自己紹介
@@ -30,9 +30,9 @@ public class UserAddRequest implements Serializable {
 
     
      //パスワード
-    @NotEmpty(message = "パスワードを入力してください")
-    @Size(min = 8, message = "パスワードは8字以上で入力してください")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]+$", message = "パスワードは数字と文字を含む必要があります")
+    @NotEmpty(message = "パスワードは必ず入力してください")
+    @Size(min = 8, message = "英数字8文字以上で入力してください")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]+$", message = "英数字8文字以上で入力してください")
     private String password;
     
  
